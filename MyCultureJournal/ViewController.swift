fcb619b720cb89567976b153b9aca3d7c5dcaa1a
yabby <yabby1997@gmail.com>
2021-09-23 23:28:11 +0900

    Initial Commit

---
+++ MyCultureJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCultureJournal
+//
+//  Created by Seunghun Yang on 2021/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
